599a11ac2fd84fc13c7687e8851ad07b88337eba
tetram2674562 <54166742+tetram2674562@users.noreply.github.com>
2026-05-24 00:53:10 +0200

    Feat: Updated example main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <typeinfo>
 
 #include "iostream"
+#include "sstream"
 #include "Yippee2.h"
 class YIPPEE {
 public:
@@ -20,6 +21,7 @@ class SuperYippee : public YIPPEE {};
 class WeakerYippee : public YIPPEE {};
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
     SuperYippee* yippee = new SuperYippee();
     Yippee(23);
     if (typeid(*yippee) == typeid(SuperYippee)) {
@@ -27,6 +29,9 @@ int main() {
     }
     delete yippee;
     // cout isn't working for some reason ?
-    std::cout << "uwu";
+    std::stringstream stream;
+    stream << "Hello YIPPEE! \n";
+    printf(stream.str().c_str());
+    std::cout << "Hello from cout !!!" << std::endl;
     return 0;
 }
